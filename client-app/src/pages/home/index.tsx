42053b5d808f80440d45655d3a9b3f9e13460195
Sam Law <samuel.law@system1group.com>
2022-10-08 14:30:25 +0100

    Added websocket comms using Socket.IO

--- client-app/src/pages/home/index.tsx
+++ client-app/src/pages/home/index.tsx
@@ -1,6 +1,11 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import io from 'socket.io-client';
+
+const socket = io('http://localhost:3001');
 
 const Home = () => {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+  const [lastPong, setLastPong] = useState(null);
   const [message, setMessage] = useState('No message set');
 
   const getData = () => {
@@ -8,6 +13,30 @@ const Home = () => {
       .then((res) => res.json())
       .then((data) => setMessage(data.message));
   };
+
+  useEffect(() => {
+    socket.on('connect', () => {
+      setIsConnected(true);
+    });
+
+    socket.on('disconnect', () => {
+      setIsConnected(false);
+    });
+
+    socket.on('pong', () => {
+      setLastPong(new Date().toISOString());
+    });
+
+    return () => {
+      socket.off('connect');
+      socket.off('disconnect');
+      socket.off('pong');
+    };
+  }, []);
+
+  const sendPing = () => {
+    socket.emit('ping');
+  }
 
   return (
     <div className="App">
@@ -25,6 +54,10 @@ const Home = () => {
         </a>
         <button onClick={getData}>Fetch data</button>
         <p>{message}</p>
+        <br />
+        <p>{`${isConnected ? 'Connected to' : 'Disconnected from'} websocket server`}</p>
+        <button onClick={sendPing}>Ping</button>
+        <p>{lastPong || '-'}</p>
       </header>
     </div>
   );
--- server/index.js
+++ server/index.js
@@ -1,12 +1,29 @@
 const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server, {
+  cors: {
+    origin: 'http://localhost:3000'
+  }
+});
+
 const port = 3001;
 
 app.get('/api', (req, res) => {
   res.send({ message: 'Hello World! My name is Sam!' });
 });
 
-app.listen(port, () => {
+io.on('connection', (socket) => {
+  console.log('A user connected');
+
+  socket.on('ping', () => {
+    io.emit('pong');
+  });
+});
+
+server.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
