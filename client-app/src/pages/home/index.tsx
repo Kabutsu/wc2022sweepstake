f0d9627decb3a2ae177508d46238685c7321ed3b
Sam Law <samuel.law@system1group.com>
2022-11-16 12:36:13 +0000

    Link to actual API

--- client-app/src/pages/home/index.tsx
+++ client-app/src/pages/home/index.tsx
@@ -17,7 +17,7 @@ enum Stage {
 
 type TProps = {};
 
-const socket = io('https://wc-2022-sweepstake-back-end.azurewebsites.net');
+const socket = io('https://wc2022sweepstake-node.azurewebsites.net/');
 
 const Home = ({}: TProps) => {
   const [isConnected, setIsConnected] = useState(socket.connected);
